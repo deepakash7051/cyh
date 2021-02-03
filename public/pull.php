b0393bef6d4b81d08d2ad8b1b6add7be1bba8735
Macrew <macrew@MT-16G-2>
2021-02-03 17:54:37 +0530

    add pull file

---
+++ public/pull.php
@@ -0,0 +1,16 @@
+<?php
+  $key = urlencode(')91@!876@m,k:"][wsd');
+
+  //exec("cd ../ && export COMPOSER_HOME='$HOME/.config/composer' && composer dump-autoload  2>&1", $output);
+
+  exec("cd ../ && git pull https://macrew_bitbucket:$key@bitbucket.org/macrewt/swa.git master 2>&1", $output);
+
+  //exec("cd ../ && php artisan migrate 2>&1", $output);
+
+  //exec("cd ../ && php artisan db:seed --class=SettingsTableSeeder 2>&1", $output);
+
+  //exec("cd ../ && php artisan cache:clear && php artisan route:cache && php artisan optimize && php artisan view:clear 2>&1", $output);
+
+  echo implode("<br>", $output);
+
+
